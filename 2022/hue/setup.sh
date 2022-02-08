d8c8fea2582f30b14fb9b0d7f36babb378c6019e
Grant Siders <gsiders@2u.com>
2022-02-07 19:58:11 -0700

    add setup script for hue dev

---
+++ 2022/hue/setup.sh
@@ -0,0 +1,7 @@
+#create app & username 
+#we only do this once
+#curl -x POST -d '{"devicetype":"RadHome"}' http://169.254.11.125/api
+#hue username: Qu-NI3EF-U0snB3NrT4qqv36kkFS09VGJyANg37Q
+export HUSER="Qu-NI3EF-U0snB3NrT4qqv36kkFS09VGJyANg37Q"
+export HADDR="169.254.11.125"
+
